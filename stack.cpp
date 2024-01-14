3f36f0ee03eef929e7a241f66f45b7f0675ad822
Dipayan Sarkar <129957226+Dipayan23@users.noreply.github.com>
2024-01-15 00:36:47 +0530

    Stack is been created

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,63 @@
 #include <iostream>
 using namespace std;
-int main(){
+class Stack
+{
+private:
+    int arr[100];
+    int top;
 
-return 0;
+public:
+    Stack()
+    {
+        top = -1;
+    };
+    void push(int value);
+    void pop();
+    int peek();
+    bool isEmpty();
+    bool isFull();
+    int getSize();
+};
+
+void Stack :: push(int value){
+    if (isFull())
+    {
+        cout<<"Stack Overflow"<<endl;
+        return;
+    }
+    arr[++top]=value;
+}
+void Stack :: pop(){
+    if (isEmpty())
+    {
+        cout<<"Stack Underflow"<<endl;
+        return;
+    }
+    top--;
+}
+int Stack :: peek(){
+    return arr[top];
+}
+bool Stack :: isEmpty(){
+    return (top<=-1);
+}
+
+bool Stack :: isFull(){
+    return (top>=100);
+}
+
+int Stack :: getSize(){
+    return (top+1);
+}
+
+int main()
+{
+    Stack S;
+    S.push(3);
+    S.push(5);
+    S.push(1);
+    cout<<S.peek();
+    S.pop();
+    cout<<S.peek();
+    return 0;
 }
